aca02fd7c61d950b7ae415523ff66a711446449d
NV-219839842984829 <116317907+NV-219839842984829@users.noreply.github.com>
2022-11-17 15:39:44 -0600

    Add files via upload

---
+++ U5_Conditionals/L2_If/if.js
@@ -0,0 +1,18 @@
+// My first conditional statement. Check if a person is 18 years or older
+let age = 20;
+if (age >= 18) {
+  console.log("You're old enough to vote, you old geezer!");
+}
+console.log("Thank you for checking your age...");
+
+// Check if you can go to the mall: grades must be >=90
+let compSciGrade = 95;
+if (compSciGrade >= 90) {
+    console.log("Good job not being a failure. You can go to the mall, I guess.")
+}
+
+// Check for driving age. If age < 16, then driving is FORBIDDEN 
+age = 7;
+if (age < 16){
+    console.log("You're not allowed to drive. Go back to kindergarten or something, kid.")
+}
